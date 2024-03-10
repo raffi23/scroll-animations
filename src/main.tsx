a3b39d80e936a571267d2aa89b1d4059024f05ab
raffi23 <raffi@rhymecode.net>
2024-03-10 18:42:12 +0400

    setup stylex instead of tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { MotionConfig, Transition } from "framer-motion";
 
 const defaultTransition: Transition = {
